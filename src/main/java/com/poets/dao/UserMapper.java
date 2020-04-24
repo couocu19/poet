1af243fe4355c13e9aa29766ed804bceba3354ad
coucou <1131088589@qq.com>
2020-04-24 19:50:55 +0800

    4.24

--- src/main/java/com/poets/dao/UserMapper.java
+++ src/main/java/com/poets/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.poets.dao;
 
 import com.poets.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,6 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    User checkLogin(@Param("username") String username, @Param("password") String password);
 }
